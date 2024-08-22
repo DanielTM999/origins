a0c7da7125ce48eb21644433256802c9aba7bc22
Daniel Teixeira Melo <108521502+DanielTM999@users.noreply.github.com>
2024-08-21 23:02:27 -0300

    Update Controller.php

--- src/props/Controller.php
+++ src/props/Controller.php
@@ -7,9 +7,6 @@
     #[Attribute(Attribute::TARGET_CLASS|Attribute::TARGET_CLASS_CONSTANT)]
     class Controller{}
 
-    #[Attribute(Attribute::TARGET_CLASS|Attribute::TARGET_CLASS_CONSTANT)]
-    class ControllerAdvice{}
-
     #[Attribute(Attribute::TARGET_METHOD)]
     class Target{
         public function __construct(public string $exception) {
