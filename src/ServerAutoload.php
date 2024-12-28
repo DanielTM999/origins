9058b524fedbe08336da69c78634fe3f4e1fa387
= <=>
2024-12-28 10:28:20 -0300

    modified:   src/ServerAutoload.php

--- src/ServerAutoload.php
+++ src/ServerAutoload.php
@@ -7,22 +7,21 @@
 
         #[Override]
         public function load(): void{
-            $dirLibrary = __DIR__;
 
-            while (strpos($dirLibrary, 'vendor') !== false) {
-                $dirLibrary = dirname($dirLibrary);
-            }
-
-            $dirBase = $dirLibrary;
-            $vendorPos = strpos($dirBase, '\vendor');
-            if ($vendorPos !== false) {
-                $dirBase = substr($dirBase, 0, $vendorPos);
-            }
-            $_ENV["base.dir"] = $dirBase;
-            $this->autoloadFromDirectory($dirBase);
-            $this->loadedFiles = array_reverse($this->loadedFiles);
-            foreach($this->loadedFiles as $file){
-                require_once $file;
+            if(isset($_ENV["enviroment"])){
+                $enviroment = $_ENV["enviroment"];
+                if($enviroment === "prod" || $enviroment === "production"){
+                    $autoload = $this->getCache();
+                    if(isset($autoload)){
+                        $this->loadElementsByCache($autoload);
+                    }else{
+                        $this->loadElements();
+                    }
+                }else{
+                    $this->loadElements();
+                }
+            }else{
+                $this->loadElements();
             }
         }
 
@@ -75,8 +74,78 @@
             }
         }
 
+        private function loadElements(){
+            $dirBase = $this->getBaseDir();
+            $_ENV["base.dir"] = $dirBase;
+            $this->autoloadFromDirectory($dirBase);
+            $this->loadedFiles = array_reverse($this->loadedFiles);
+            foreach($this->loadedFiles as $file){
+                require_once $file;
+            }
+
+            $this->addCache([
+                "baseDir" => $dirBase,
+                "loadedFiles" => $this->loadedFiles
+            ]);
+        }
+
+        private function loadElementsByCache($cache){
+            $baseDir = $cache["baseDir"];
+            if(isset($baseDir)){
+                $_ENV["base.dir"] = $baseDir;
+            }else{
+                $_ENV["base.dir"] = $this->getBaseDir();
+            }
+            $loadedFiles = $cache["loadedFiles"];
+            if(isset($loadedFiles)){
+                foreach($loadedFiles as $file){
+                    require_once $file;
+                }
+            }else{
+                $this->loadElements();
+            }
+        }
+
+        private function getBaseDir(): string{
+            $dirLibrary = __DIR__;
+
+            while (strpos($dirLibrary, 'vendor') !== false) {
+                $dirLibrary = dirname($dirLibrary);
+            }
+
+            $dirBase = $dirLibrary;
+            $vendorPos = strpos($dirBase, '\vendor');
+            if ($vendorPos !== false) {
+                $dirBase = substr($dirBase, 0, $vendorPos);
+            }
+            return $dirBase;
+        }
+
         private function containsClassView($directory) {
             return preg_match('/\bviews?\b/', $directory);
+        }
+
+        private function getCache(){
+            $filePath = './autoload.json';
+            if (!file_exists($filePath)) {
+                return null;
+            }
+            $jsonData = file_get_contents($filePath);
+            if ($jsonData === false) {
+                return null;
+            }
+            $data = json_decode($jsonData, true);
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                return null;
+            }
+            return $data;
+        }
+
+        private function addCache($settings){
+            $jsonData = json_encode($settings, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            $filePath = './autoload.json';
+
+            file_put_contents($filePath, $jsonData);
         }
     } 
 
